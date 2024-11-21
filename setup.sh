18e1094e133173c690fae8241baa94e386d5a7d3
Anthony Gilbert <anthony.gilbert93@gmail.com>
2024-11-21 03:38:11 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
-sudo su && \\
-apt-get update -y && \\
-apt-get install -y software-properties-common && \\
-apt install ifconfig && \\
-ifconfig
+#!/bin/bash
+
+# Update Ubuntu Script
+
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or use sudo."
+  exit 1
+fi
+
+echo "Updating package lists..."
+sudo apt update
+
+echo "Upgrading packages..."
+sudo apt upgrade -y
+
+echo "Performing distribution upgrade..."
+sudo apt dist-upgrade -y
+
+echo "Removing unnecessary packages..."
+sudo apt autoremove -y
+
+echo "Cleaning up package cache..."
+sudo apt autoclean -y
+
+echo "Update complete!"
